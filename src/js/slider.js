825680a9ae73d0d88f0bcd06bcbd9a626fea67a7
Mathew <mathewhleland@gmail.com>
2018-03-01 16:27:36 -0800

    slider own module that depends on data's length for values

---
+++ src/js/slider.js
@@ -0,0 +1,20 @@
+export const Slider = function(divId, length) {
+  //id is the element id we are going to append to
+  // data is the list of items (probably arrays) that the slider will pull from
+  const id = divId || "slider";
+
+  const min = 0;
+  const max = length;
+  const start = Math.floor(length / 3);
+
+  document.getElementById(id).innerHTML = `
+    <input 
+      type='range' 
+      min=${min} 
+      max=${max} 
+      value=${start}
+      class='slider'
+      id='pickDate'
+    >
+  `;
+};
