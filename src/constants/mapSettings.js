ff2510d819f5593c1f1fef02e48e45704a0acae0
Mathew <mathewhleland@gmail.com>
2018-03-05 16:35:35 -0800

    take streetview off map, adjust view

--- src/constants/mapSettings.js
+++ src/constants/mapSettings.js
@@ -176,9 +176,11 @@ export const myMapStyles = [
 ];
 
 export const mapOptions = {
-  center: { lat: 35.67, lng: -119.1 },
+  center: { lat: 35.87, lng: -119.5 },
   zoom: 8,
   mapTypeControl: false,
   scrollwheel: false,
+  fullscreenControl: false,
+  streetViewControl: false,
   styles: myMapStyles
 };
