6c81433a9be2b0c54efacb4a62e4367df4880d60
Mathew <mathewhleland@gmail.com>
2018-03-16 17:24:14 -0700

    create production build webpack file

---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/zika.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.js$/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [new UglifyJSPlugin()]
+};
