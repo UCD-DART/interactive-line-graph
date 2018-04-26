6fcf1ae14d13f90e238fd5794a22724390478bef
Mathew <mathewhleland@gmail.com>
2018-04-26 16:10:43 -0700

    modify bundled data to start using self-built api

--- src/js/data.js
+++ src/js/data.js
@@ -16,97 +16,132 @@ const service = {
     }),
   getDataById: id =>
     axios({
-      url: `https://calsurv.herokuapp.com/api/zika/${id}`,
+      url: `http://mathew.calsurv.org/api/zika/${id}`,
       validateStatus: function(status) {
         return status < 500;
       }
     })
 };
 
-async function getData() {
+async function getMyData() {
   let finalFeatures = [];
+
   try {
-    let layer = await service.getFeatures();
+    const layer = await service.getFeatures(); // get an initial list of all cities in the gateway
     let features = layer.data.features;
 
     for (let i = 0; i < features.length; i++) {
       let feature = features[i];
-      const city = feature.properties.city;
-      // const id = feature.id;
-      let cityData = await service.getData(city);
+      const id = feature.id;
+      let cityData = await service.getDataById(id);
 
       if (cityData.data.length > 1 && cityData.status === 200) {
-        feature.properties.risk = formatData(cityData.data);
+        let riskObj = cityData.data.map(datum => {
+          return {
+            date: datum.date,
+            risk: datum.risk
+          };
+        });
+        feature.properties.risk = riskObj;
         finalFeatures.push(feature);
-        // console.log("pushed feature is ");
-        console.log(city);
+        console.log(id);
       }
     }
-    // console.log(finalFeatures);
+
     geoJson = {
       type: "FeatureCollection",
       features: finalFeatures
     };
-    fs.writeFile("./risk.json", JSON.stringify(geoJson), function(err) {
-      if (err) console.log("writing to the disk did not work");
+    fs.writeFile("./risk2.json", JSON.stringify(geoJson), function(err) {
+      if (err) console.log("writing to disk did not work");
     });
     console.log(
-      "💣🚀🚀 FINISHED " + finalFeatures.length + " 🏙️ 🏛️ 🏠   cities in json"
+      "Finished 💣💣_ ahh yess!!  it worked.  Geojson data contains " +
+        finalFeatures.length +
+        " cities in the file"
     );
   } catch (err) {
-    console.log("getData() error: " + err);
+    console.log("getData function error: " + err);
   }
 }
 
-getData();
+getMyData();
 
-var format = d3.timeFormat("%Y-%m-%d");
+// async function getData() {
+//   let finalFeatures = [];
+//   try {
+//     let layer = await service.getFeatures();
+//     let features = layer.data.features;
 
-// function formatData2(arr) { // for use with my calsurv api on heroku -- already has values averaged by week
-//   return arr.map(function(datum) {
-//     return {
-//       date: datum.date,
-//       risk: datum.risk
+//     for (let i = 0; i < features.length; i++) {
+//       let feature = features[i];
+//       const city = feature.properties.city;
+//       // const id = feature.id;
+//       let cityData = await service.getData(city);
+
+//       if (cityData.data.length > 1 && cityData.status === 200) {
+//         feature.properties.risk = formatData(cityData.data);
+//         finalFeatures.push(feature);
+//         // console.log("pushed feature is ");
+//         console.log(city);
+//       }
+//     }
+//     // console.log(finalFeatures);
+//     geoJson = {
+//       type: "FeatureCollection",
+//       features: finalFeatures
 //     };
-//   });
+//     fs.writeFile("./risk.json", JSON.stringify(geoJson), function(err) {
+//       if (err) console.log("writing to the disk did not work");
+//     });
+//     console.log(
+//       "💣🚀🚀 FINISHED " + finalFeatures.length + " 🏙️ 🏛️ 🏠   cities in json"
+//     );
+//   } catch (err) {
+//     console.log("getData() error: " + err);
+//   }
 // }
 
-function formatData(city) {
-  let bucketDate = new Date("2016-01-01T00:00:00.000Z");
+// // getData();
 
-  Date.prototype.addDays = function(days) {
-    const date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
-  };
+// var format = d3.timeFormat("%Y-%m-%d");
 
-  let final = [[format(bucketDate), []]];
-  let bucket = 0; // bucket
-  for (let i = 0; i < city.length; i++) {
-    let currentDate = new Date(city[i][0]);
-    if (currentDate < bucketDate.addDays(7)) {
-      //WITHIN 7 DAYS
-      final[bucket][1].push(+city[i][1]);
-    } else {
-      // MORE THAN 7 DAYS
-      //Assign new bucket date, create new bucket with it and its first value is the current city value
-      bucketDate = bucketDate.addDays(7);
-      bucket++;
-      final.push([format(bucketDate), [+city[i][1]]]);
-    }
-  }
-  //calculate the avg risk for each bucket, format as object for API
-  final = final.map(week => {
-    let obj = {};
-    const avg =
-      week[1].reduce(function(a, b) {
-        return a + b;
-      }, 0) / week[1].length;
-    obj["date"] = new Date(week[0]);
-    obj["risk"] = +avg.toFixed(3);
-    return obj;
-  });
-  return final;
-}
+// function formatData(city) {
+//   let bucketDate = new Date("2016-01-01T00:00:00.000Z");
+
+//   Date.prototype.addDays = function(days) {
+//     const date = new Date(this.valueOf());
+//     date.setDate(date.getDate() + days);
+//     return date;
+//   };
+
+//   let final = [[format(bucketDate), []]];
+//   let bucket = 0; // bucket
+//   for (let i = 0; i < city.length; i++) {
+//     let currentDate = new Date(city[i][0]);
+//     if (currentDate < bucketDate.addDays(7)) {
+//       //WITHIN 7 DAYS
+//       final[bucket][1].push(+city[i][1]);
+//     } else {
+//       // MORE THAN 7 DAYS
+//       //Assign new bucket date, create new bucket with it and its first value is the current city value
+//       bucketDate = bucketDate.addDays(7);
+//       bucket++;
+//       final.push([format(bucketDate), [+city[i][1]]]);
+//     }
+//   }
+//   //calculate the avg risk for each bucket, format as object for API
+//   final = final.map(week => {
+//     let obj = {};
+//     const avg =
+//       week[1].reduce(function(a, b) {
+//         return a + b;
+//       }, 0) / week[1].length;
+//     obj["date"] = new Date(week[0]);
+//     obj["risk"] = +avg.toFixed(3);
+//     return obj;
+//   });
+//   return final;
+// }
 
 module.exports = geoJson;
